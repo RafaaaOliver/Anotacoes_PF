3a0cbe0a6c0ad076fe12ecc831806b0b91c68291
Rafael Oliveira <99557624+RafaaaOliver@users.noreply.github.com>
2023-05-30 14:59:40 +0000

    Oracle or Bank

--- Fluxo de cadastro de clientes/pf_placeholder_cs.js
+++ Fluxo de cadastro de clientes/pf_placeholder_cs.js
@@ -5,7 +5,46 @@
 define([], function () {
 
     function pageInit(context) {
-        definePlaceHolder('custrecord_pf_solicitacao_field_obs', 'Digite a alteração...')
+        try {
+            definePlaceHolder('custrecord_pf_solicitacao_field_obs', 'Digite a alteração...')
+        } catch (e) {
+            console.error(`Erro: ${e}`)
+        }
+    }
+
+    function fieldChanged(context) {
+        try {
+            const fieldId = context.fieldId
+            const CurrentRecord = context.currentRecord
+            var banco = CurrentRecord.getField('custrecord_pf_solicitacao_field_bancos')
+            var oracle = CurrentRecord.getField('custrecord_pf_solicitacao_field_oracle')
+            var valueBanco = CurrentRecord.getValue('custrecord_pf_solicitacao_field_bancos')
+            var valueOracle = CurrentRecord.getValue('custrecord_pf_solicitacao_field_oracle')
+            console.log(`ValueBanco: ${valueBanco}`)
+            console.log(`valueOracle: ${valueOracle}`)
+            console.log(`fieldId: ${fieldId}`)
+
+            if (fieldId == banco.id && valueBanco) {
+                console.log('Desabilitei Oracle')
+                oracle.isDisabled = true
+            }
+            if (fieldId == banco.id && valueBanco == ''){
+                console.log('Habilitei Oracle')
+                oracle.isDisabled = false
+            }
+
+            if (fieldId == oracle.id && valueOracle) {
+                console.log('Desabilitei Banco')
+                banco.isDisabled = true
+            }
+            if (fieldId == oracle.id && !valueOracle){
+                console.log('Habilitei Banco')
+                banco.isDisabled = false
+            }
+            
+        } catch (e) {
+            console.error(`Erro: ${e}`)
+        }
     }
 
     const definePlaceHolder = (field, text) => {
@@ -14,6 +53,7 @@ define([], function () {
     }
 
     return {
-        pageInit: pageInit
+        pageInit: pageInit,
+        fieldChanged: fieldChanged
     }
 });
